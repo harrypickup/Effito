ea91a5c3f22be6c57d5f20252467bbc3e78debbc
harrypickup <harrypickup1@hotmail.com>
2026-03-24 15:16:57 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 
 // This finds the "root" div in your index.html
 const rootElement = document.getElementById('root');

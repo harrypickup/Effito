6d9b28695c954cd4837e894b5f289958452b817a
harrypickup <harrypickup1@hotmail.com>
2026-03-25 09:58:15 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import Challenges from './pages/Challenges';
 import Infrastructure from './pages/Infrastructure';
 import Performance from './pages/Performance';
-import HowItWorks from './pages/HowItWorks';
+import Logic from './pages/Logic';
 import Contact from './pages/Contact';
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
@@ -32,7 +32,7 @@ const App: React.FC = () => {
             <Route path="/challenges" element={<Challenges />} />
             <Route path="/infrastructure" element={<Infrastructure />} />
             <Route path="/performance" element={<Performance />} />
-            <Route path="/how-it-works" element={<HowItWorks />} />
+            <Route path="/logic" element={<Logic />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
